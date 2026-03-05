de34d244ddd487fd37172f4723d27d11c1be9c1f
0xThomas <khawtom2541@gmail.com>
2026-03-06 03:20:07 +0700

    Improve CORS logging for debugging

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -1,6 +1,6 @@
 const cors = require('cors');
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS 
+const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(',')
   : ['http://localhost:5173', 'http://localhost:3000'];
 
@@ -8,10 +8,12 @@ const corsOptions = {
   origin: function (origin, callback) {
     // Allow requests with no origin (like mobile apps or curl)
     if (!origin) return callback(null, true);
-    
+
     if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
+      console.log('CORS blocked origin:', origin);
+      console.log('Allowed Origins:', allowedOrigins);
       callback(new Error('Not allowed by CORS'));
     }
   },
